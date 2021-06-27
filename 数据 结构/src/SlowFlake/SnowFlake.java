d60ee9eb26f1899c7a1bcf8b12f05fc88fb99efc
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-06-27 16:04:45 +0800

    Update SnowFlake.java

--- 数据 结构/src/SlowFlake/SnowFlake.java
+++ 数据 结构/src/SlowFlake/SnowFlake.java
@@ -1,22 +1,6 @@
 package SlowFlake;
 
-/***
-LICENSE
-Copyright 北大出版社
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-*/
+/**雪花算法来自网上搜索的，主要是用来自增ID的 */
 
 /*
 	本例代码来自于对Twitter snowflake的修改，原创代码请参阅以下网址：
